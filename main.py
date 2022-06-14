ed2e3c3f9c26cbc45209a240e243d4df3de23e36
Brenda Orlandi <brenda.orlandi@ufvjm.edu.br>
2022-06-14 17:04:44 -0300

    Primeiro commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Criar professor
+
 import gurobipy as gp
 import pandas as pd
 
@@ -26,12 +28,16 @@ m = gp.Model()
 x = m.addVars(dia, materia, horario, vtype=gp.GRB.BINARY)
 
 """Criação da Função Objetivo"""
-m.setObjective(x.sum('*', '*', '*'), sense=gp.GRB.MAXIMIZE)
+m.setObjective(x.sum('*', '*', '*'), sense=gp.GRB.MINIMIZE)
 
 """ Criação das Restrições """
+
+"""Cada matéria tem uma carga horária"""
 c1 = m.addConstrs(x.sum('*', j, '*') == carga_horaria[j] for j in materia)
+"Em um determinado dia e horario, terá no máximo uma matéria"
 c2 = m.addConstrs(x.sum(i, '*', k) <= 1 for i in dia for k in horario)
-c3 = m.addConstrs(x.sum('*', j, k) <= 1 for j in materia for k in horario)
+"Em uma determinada matéria e horario, terá no máximo uma matéria"
+# c3 = m.addConstrs(x.sum('*', j, k) <= 1 for j in materia for k in horario)
 
 """Executa o modelo"""
 m.optimize()
