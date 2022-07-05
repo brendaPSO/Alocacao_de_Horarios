3ce1979bac3a9408a1fff4533749ed457ac2a83f
Brenda Orlandi <brenda.orlandi@ufvjm.edu.br>
2022-07-05 15:48:51 -0300

    V3: restrição horário

--- v3/main.py
+++ v3/main.py
@@ -19,11 +19,14 @@ model.setObjective(gp.quicksum(x[p, m, h, d] for p in professores for m in mater
 """Cada matéria do professor tem uma carga horária"""
 c1 = model.addConstrs(x.sum(prof_materia[m].strip(), m, '*', '*') == carga_materia[m] for m in materias)
 
-"Em uma determinada materia, horario e dia, terá no máximo um professor"
-c2 = model.addConstrs(x.sum('*', m, h, '*') <= 1 for m in materias for h in horarios)
+# "Em uma determinada materia, horario e dia, terá no máximo um professor"
+# c2 = model.addConstrs(x.sum('*', m, h, d) <= 1 for m in materias for h in horarios for d in dias)
 
-"Em um determinado professor, horario e dia, terá no máximo uma matéria"
-c3 = model.addConstrs(x.sum(p, '*', h, d) <= 1 for p in professores for h in horarios for d in dias)
+# "Em um determinado professor, horario e dia, terá no máximo uma matéria"
+# c3 = model.addConstrs(x.sum(p, '*', h, d) <= 1 for p in professores for h in horarios for d in dias)
+
+""" Em um determinado dia e horário terá no máximo 1 matéria/professor"""
+c4 = model.addConstrs(x.sum('*', '*', h, d) <= 1 for h in horarios for d in dias)
 
 "Incentivo as matérias com horários consecultivos"
 
