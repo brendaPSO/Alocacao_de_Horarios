4d59f5546d6222f8aa6d4dfe46ce8536946470ec
Brenda Orlandi <brenda.orlandi@ufvjm.edu.br>
2022-07-05 14:03:07 -0300

    gitignore

---
+++ v3/Database/organizarDados.py
@@ -0,0 +1,31 @@
+from v1.entity import MateriaClass, ProfessorClass
+import pandas as pd
+
+
+class Dados:
+
+    @staticmethod
+    def organizarDados():
+        dataProfessor = pd.read_csv('Database/professor.csv', sep=',')
+        dataMateria = pd.read_csv('Database/materia.csv', sep=',')
+
+        vet_professores = list(dataProfessor.values.base[0])
+        vet_materia_nome = list(dataMateria.values.base[0])
+        vet_materia_professor = list(dataMateria.values.base[1])
+        vet_materia_carga = list(dataMateria.values.base[2])
+        horarios = ['18h', '19h', '20h', '21h', '22h']
+        dia = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta']
+
+        listProfessor = []
+        for idx, professor in enumerate(vet_professores):
+            listProfessor.append(professor)
+
+        prof_materia = dict()
+        carga = dict()
+        listMateria = []
+        for idx, materia in enumerate(vet_materia_nome):
+            prof_materia[materia] = vet_materia_professor[idx]
+            carga[materia] = vet_materia_carga[idx]
+            listMateria.append(materia)
+
+        return prof_materia, horarios, carga, listMateria, listProfessor, dia
---
+++ v3/main.py
@@ -0,0 +1,39 @@
+from Database.organizarDados import Dados
+import gurobipy as gp
+
+[prof_materia, horarios, carga_materia, materias, professores, dias] = Dados.organizarDados()
+
+model = gp.Model()
+
+# ====== Variáveis de Decisão
+x = model.addVars(professores, materias, horarios, dias, vtype=gp.GRB.BINARY)
+
+
+# ====== Criação da Função Objetivo
+model.setObjective(gp.quicksum(x[p, m, h, d] for p in professores for m in materias for h in horarios for d in dias),
+                   sense=gp.GRB.MINIMIZE)
+
+
+# ====== Criação das Restrições
+
+"""Cada matéria do professor tem uma carga horária"""
+c1 = model.addConstrs(x.sum(prof_materia[m].strip(), m, '*', '*') == carga_materia[m] for m in materias)
+
+"Em uma determinada materia, horario e dia, terá no máximo um professor"
+c2 = model.addConstrs(x.sum('*', m, h, '*') <= 1 for m in materias for h in horarios)
+
+"Em um determinado professor, horario e dia, terá no máximo uma matéria"
+c3 = model.addConstrs(x.sum(p, '*', h, d) <= 1 for p in professores for h in horarios for d in dias)
+
+"Incentivo as matérias com horários consecultivos"
+
+
+# ====== Executa o modelo
+model.optimize()
+
+for p in professores:
+    for m in materias:
+        for h in horarios:
+            for d in dias:
+                if round(x[p, m, h, d].X) == 1:
+                    print(p, m, h, d)
