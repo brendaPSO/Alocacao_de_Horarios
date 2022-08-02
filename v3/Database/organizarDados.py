467aeb6abcb8260893da81918280e01d57c7d142
Brenda Orlandi <brenda.orlandi@ufvjm.edu.br>
2022-08-02 01:06:42 -0300

    Dias disponíveis - professores

--- v3/Database/organizarDados.py
+++ v3/Database/organizarDados.py
@@ -1,4 +1,3 @@
-from v1.entity import MateriaClass, ProfessorClass
 import pandas as pd
 
 
@@ -6,9 +5,8 @@ class Dados:
 
     @staticmethod
     def organizarDados():
-        dataProfessor = pd.read_csv('Database/professor.csv', sep=',')
+        dataProfessor = pd.read_csv('Database/professor.csv', sep=';')
         dataMateria = pd.read_csv('Database/materia.csv', sep=',')
-
         vet_professores = list(dataProfessor.values.base[0])
         vet_materia_nome = list(dataMateria.values.base[0])
         vet_materia_professor = list(dataMateria.values.base[1])
@@ -16,9 +14,21 @@ class Dados:
         horarios = ['18h', '19h', '20h', '21h', '22h']
         dia = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta']
 
-        listProfessor = []
-        for idx, professor in enumerate(vet_professores):
-            listProfessor.append(professor)
+        dataDia = open('Database/dia.txt', 'r')
+        linhas = dataDia.readlines()
+        dataDia.close()
+
+        prof_dia = dict()
+        campos = list()
+        i = 0
+        for linha in linhas:
+            if linha == "\n":
+                continue
+            campos.append(linha.replace("\n", "").split(","))
+            if (i+1) % 5 == 0:
+                prof_dia[vet_professores[(i/5).__trunc__()]] = campos.copy()
+                campos.clear()
+            i = i+1
 
         prof_materia = dict()
         carga = dict()
@@ -28,4 +38,4 @@ class Dados:
             carga[materia] = vet_materia_carga[idx]
             listMateria.append(materia)
 
-        return prof_materia, horarios, carga, listMateria, listProfessor, dia
+        return prof_materia, horarios, carga, listMateria, vet_professores, dia, prof_dia
--- v3/main.py
+++ v3/main.py
@@ -1,13 +1,18 @@
 from Database.organizarDados import Dados
 import gurobipy as gp
+import numpy as np
 
-[prof_materia, horarios, carga_materia, materias, professores, dias] = Dados.organizarDados()
+[prof_materia, horarios, carga_materia, materias, professores, dias, prof_dia] = Dados.organizarDados()
+
+for i, p in enumerate(professores):
+    for j, d in enumerate(dias):
+        for k, h in enumerate(horarios):
+            print(prof_dia[p][j][k])
 
 model = gp.Model()
 
 # ====== Variáveis de Decisão
 x = model.addVars(professores, materias, horarios, dias, vtype=gp.GRB.BINARY)
-
 
 # ====== Criação da Função Objetivo
 model.setObjective(gp.quicksum(x[p, m, h, d] for p in professores for m in materias for h in horarios for d in dias),
@@ -26,7 +31,14 @@ c1 = model.addConstrs(x.sum(prof_materia[m].strip(), m, '*', '*') == carga_mater
 # c3 = model.addConstrs(x.sum(p, '*', h, d) <= 1 for p in professores for h in horarios for d in dias)
 
 """ Em um determinado dia e horário terá no máximo 1 matéria/professor"""
-c4 = model.addConstrs(x.sum('*', '*', h, d) <= 1 for h in horarios for d in dias)
+c2 = model.addConstrs(x.sum('*', '*', h, d) <= 1 for h in horarios for d in dias)
+
+""" Em um determinado dia e horário terá no máximo 1 matéria/professor"""
+c3 = model.addConstrs(x.sum(p, '*', h, d) <= int(prof_dia[p][j][k]) for i, p in enumerate(professores)
+                      for j, d in enumerate(dias) for k, h in enumerate(horarios))
+
+
+
 
 "Incentivo as matérias com horários consecultivos"
 
